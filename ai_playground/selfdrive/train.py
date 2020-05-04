43400574f90e5f349165b96bded71864ce215830
adrianB3 <balanescuadrian71@gmail.com>
2020-05-04 08:38:05 +0300

    update

--- ai_playground/selfdrive/train.py
+++ ai_playground/selfdrive/train.py
@@ -5,6 +5,7 @@ import click
 import tensorflow as tf
 import sys
 
+from tf_agents import environments
 from tf_agents.agents.tf_agent import TFAgent
 from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.environments import tf_py_environment
@@ -25,11 +26,13 @@ class AlgorithmManager:
     def __init__(self, ctx: click.Context):
         self.ctx = ctx
         self.tf_env: tf_py_environment.TFPyEnvironment = tf_py_environment.TFPyEnvironment(SelfDriveEnvironment(ctx))
+        # environments.utils.validate_py_environment(self.tf_env)
         self.train_step_counter = tf.compat.v1.train.get_or_create_global_step()
         self.tf_agent: TFAgent = self.get_agent(
             algorithm_type=ctx.obj['config']['algorithm']['name']
         )
         self.tf_agent.initialize()
+
 
     def get_agent(self, algorithm_type: str):
         if algorithm_type == 'ppo':
@@ -82,35 +85,53 @@ class Trainer:
         sys.exit(0)
 
     def train(self):
-        env_steps_metric = tf_metrics.EnvironmentSteps()
-        env_steps_metric.init_variables()
         step_metrics = [
             tf_metrics.NumberOfEpisodes(),
-            env_steps_metric
+            tf_metrics.EnvironmentSteps(),
+            tf_metrics.AverageReturnMetric(),
+            tf_metrics.AverageEpisodeLengthMetric()
         ]
-
+        replay_observer = [self.replay_buffer.add_batch]
+        dataset = self.replay_buffer.as_dataset(
+            num_parallel_calls=4,
+            sample_batch_size=1,
+            num_steps=2
+        ).prefetch(3)
+        iterator = iter(dataset)
         collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
             env=self.algo_manager.tf_env,
             policy=self.algo_manager.tf_agent.collect_policy,
-            observers=[self.replay_buffer.add_batch],
+            observers=replay_observer + step_metrics,
             num_episodes=self.ctx.obj['config']['train_session']['collect_eps_per_iter']
         )
+        self.algo_manager.tf_agent.train_step_counter.assign(0)
         collect_time = 0
         train_time = 0
-        try:
-            while env_steps_metric.result() < self.ctx.obj['config']['train_session']['num_env_steps']:
-                start_time = time.time()
+        returns = []
+        for _ in range(self.ctx.obj['config']['train_session']['num_env_steps']):
+            for _ in range(self.ctx.obj['config']['train_session']['collect_eps_per_iter']):
                 collect_driver.run()
-                collect_time += time.time() - start_time
+            experience, unused_info = next(iterator)
+            train_loss = self.algo_manager.tf_agent.train(experience)
 
-                start_time = time.time()
-                trajectories = self.replay_buffer.gather_all()
-                total_loss, _ = self.algo_manager.tf_agent.train(experience=trajectories)
-                self.replay_buffer.clear()
-                train_time += time.time() - start_time
-                env_steps_metric.call(trajectories)
-                self.save_artifacts()
-                if self.ctx.obj['log2neptune']:
-                    neptune.log_metric("collect_time", env_steps_metric.result(), collect_time)
-        except (KeyboardInterrupt, SystemExit):
-            self.exit_gracefully()
+            step = self.algo_manager.tf_agent.train_step_counter.numpy()
+            self.save_artifacts()
+            if self.ctx.obj['log2neptune']:
+                pass
+
+        # try:
+        #     while env_steps_metric.result() < :
+        #         start_time = time.time()
+        #         collect_driver.run()
+        #         collect_time += time.time() - start_time
+        #
+        #         start_time = time.time()
+        #         trajectories = self.replay_buffer.gather_all()
+        #         total_loss, _ = self.algo_manager.tf_agent.train(experience=trajectories)
+        #         self.replay_buffer.clear()
+        #         train_time += time.time() - start_time
+        #
+        #
+        #             neptune.log_metric("collect_time", collect_time)
+        # except (KeyboardInterrupt, SystemExit):
+        #     self.exit_gracefully()
